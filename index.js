45a4c73e0218d899fc599bd475cabdbecdadeffe
rc.github.io <rachelcheniphone@gmail.com>
2026-02-24 23:39:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,43 +4,33 @@ const cors = require('cors');
 const app = express();
 const dns = require('dns');
 const urlParser = require('url');
-
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(`${process.cwd()}/public`));
-
-
-app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
 let urlDatabase = [];
 let idCounter = 1;
 
-
-
 app.post('/api/shorturl', (req, res) => {
   const originalUrl = req.body.url;
-  
-  
   const parsedUrl = urlParser.parse(originalUrl);
-
 
   if (!parsedUrl.protocol || !/^https?:/.test(parsedUrl.protocol)) {
     return res.json({ error: 'invalid url' });
   }
 
-  // Use DNS lookup to verify the site actually exists
   dns.lookup(parsedUrl.hostname, (err) => {
     if (err) {
       res.json({ error: 'invalid url' });
     } else {
-      // Test 2: Store and return the JSON
       const shortUrl = idCounter++;
       urlDatabase.push({ original: originalUrl, short: shortUrl });
       
@@ -61,12 +51,6 @@ app.get('/api/shorturl/:id', (req, res) => {
   } else {
     res.json({ error: "No short URL found" });
   }
-});
-
-
-
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
 });
 
 app.listen(port, function() {
